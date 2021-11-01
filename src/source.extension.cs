b14d080a170442676cd25bd184af6f37f8b66d07
Jude Vajira Gunasekera <judevajira@gmail.com>
2021-11-01 09:42:29 +0530

    updated guids

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -7,12 +7,12 @@ namespace MadsKristensen.AddAnyFile
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "d48e8f25-661b-4970-8b66-03f051ba5fc3";
-        public const string Name = "Add New File (64-bit)";
+        public const string Id = "7bf9a1b6-a1b0-4cc6-acad-684e0553d01b";
+        public const string Name = "Add New File Plus (64-bit)";
         public const string Description = @"The fastest and easiest way to add new files to any project - including files that start with a dot";
         public const string Language = "en-US";
-        public const string Version = "4.0";
-        public const string Author = "Mads Kristensen";
+        public const string Version = "4.0.1";
+        public const string Author = "Jude Vajira";
         public const string Tags = "file, add, template";
     }
 }
