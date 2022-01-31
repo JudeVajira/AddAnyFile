cc5906213b425a6cbb550e97f4e40fedd9d9c601
Jude Vajira Gunasekera <judevajira@gmail.com>
2022-01-31 11:03:21 +0530

    renamed product name

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace MadsKristensen.AddAnyFile
     internal sealed partial class Vsix
     {
         public const string Id = "7bf9a1b6-a1b0-4cc6-acad-684e0553d01b";
-        public const string Name = "Add New File Plus (64-bit)";
+        public const string Name = "Add New File Plus (64-bit) - Jude Edition";
         public const string Description = @"The fastest and easiest way to add new files to any project - including files that start with a dot";
         public const string Language = "en-US";
         public const string Version = "4.0.1";
